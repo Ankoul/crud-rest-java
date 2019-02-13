84a7dbadb15dee70819d0486c231a13fb61cb8d1
gilson <gilson@intuitiva.net.br>
2019-02-13 15:27:54 -0200

    remove datasource

--- src/main/java/br/net/intuitiva/crud/api/Application.java
+++ src/main/java/br/net/intuitiva/crud/api/Application.java
@@ -1,11 +1,11 @@
 package br.net.intuitiva.crud.api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
-@EnableAutoConfiguration
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
