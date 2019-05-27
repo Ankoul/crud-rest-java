6cdf3343c09acfffac3dc31cdc411633139f79da
gilson <gilson@intuitiva.net.br>
2019-05-27 18:17:04 -0300

    version 1.2

--- src/main/java/br/net/intuitiva/crud/api/Application.java
+++ src/main/java/br/net/intuitiva/crud/api/Application.java
@@ -1,12 +1,25 @@
 package br.net.intuitiva.crud.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
+
+	@Value("${project.version}")
+	private String version;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getVersion() {
+		return version;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
